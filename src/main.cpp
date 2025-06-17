1bf088d24c4bf29eb80c114da1322223ab243831
Avesta <avesta.alamoutifard85@gmail.com>
2025-06-17 12:36:51 +0330

    test main added

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include "./src/ui/mainwindow.h"
-
+#include "./src/backend/db/DBM.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    DBM::get_instance();
+
     MainWindow w;
+
     w.show();
+
     return a.exec();
 }
